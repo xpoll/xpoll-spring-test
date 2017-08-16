0a58b4cd885a370a0a2c8ec3a9aec4f4b8445264
xpoll <blmdz521@126.com>
2017-08-16 22:42:20 +0800

    fff

---
+++ src/test/java/cn/blmdz/local/MessTest.java
@@ -0,0 +1,49 @@
+package cn.blmdz.local;
+
+import java.text.DateFormat;
+import java.text.MessageFormat;
+import java.text.NumberFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class MessTest {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public void test(){
+		
+		NumberFormat nfmt = NumberFormat.getCurrencyInstance(Locale.SIMPLIFIED_CHINESE);
+		System.out.println(nfmt.format(1.23324));
+		
+		DateFormat dfmt = DateFormat.getDateInstance(DateFormat.DEFAULT, Locale.ENGLISH);
+		System.out.println(dfmt.format(new Date()));
+		
+		String p1 = "{0}，你好！你于{1}在工商银行存入{2}元。";
+		String p2 = "At {1,time,short} On {1,date,long}, {0} paid {2,number,currency}.";
+		Object[] params = {"John", new GregorianCalendar().getTime(), 1.0E3};
+		
+		String msg1 = MessageFormat.format(p1, params);
+		System.out.println(msg1);
+		
+		MessageFormat mf = new MessageFormat(p2, Locale.US);
+		System.out.println(mf.format(params));
+
+		ResourceBundle rb1 = ResourceBundle.getBundle("resource");
+		ResourceBundle rb2 = ResourceBundle.getBundle("resource", Locale.CHINA);
+		ResourceBundle rb3 = ResourceBundle.getBundle("resource", Locale.US);
+		ResourceBundle rb4 = ResourceBundle.getBundle("resource", Locale.CANADA);
+		System.out.println(rb1.getString("a"));
+		System.out.println(rb2.getString("a"));
+		System.out.println(rb3.getString("a"));
+		System.out.println(rb4.getString("a"));
+		
+//		HierarchicalMessageSource
+//		ReloadableResourceBundleMessageSource
+//		ResourceBundleMessageSource
+	}
+
+}
